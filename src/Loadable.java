6fade19eb269de5d195e62ddb80db71cd5fcb5cf
Legetic <Legetic@hotmail.com>
2018-11-22 23:05:58 +0100

    javadocs for loadable

--- src/Loadable.java
+++ src/Loadable.java
@@ -1,9 +1,28 @@
-public interface Loadable <T> {
+/**
+ * Interface with methods for loading and unloading objects as well as checking if the <i>Loadable</i> object is full.
+ * Should be implemented by all objects that load and unload other objects.
+ * @param <T> arbitrary type of object to be loaded.
+ * @author Matte B, Arre S, Alle V
+ * @version 1.0
+ * @since 2018-11-07
+ */
+public interface Loadable<T> {
 
-     void load(T t);
+    /**
+     * Loads the loadable with an object of arbitrary type.
+     * @param t object to be loaded.
+     */
+    void load(T t);
 
+    /**
+     * Checks whether the loadable is full or not.
+     * @return true if loadable is full.
+     */
     boolean isFull();
 
+    /**
+     * Unloads an object of arbitrary type from the loadable.
+     */
     void unload();
 
 
